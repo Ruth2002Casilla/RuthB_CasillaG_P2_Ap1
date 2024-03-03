358e4ba037fe53c533f64820e695dd94ee3cee5d
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-03-03 17:54:34 -0400

    Agregando el contexto

---
+++ SegundoParcial/SegundoParcial.Server/DAL/Context.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SegundoParcial.Server.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context>options) : base(options) { }
+    }
+}
