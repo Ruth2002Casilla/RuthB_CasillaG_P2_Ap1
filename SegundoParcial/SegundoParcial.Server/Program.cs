07e544e6ff163608ff8e2fb146b8f6249b5180d3
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-03-04 19:47:11 -0400

    CRUD de Vehiculos cambiados de carpeta y conexion hecha

--- SegundoParcial/SegundoParcial.Server/Program.cs
+++ SegundoParcial/SegundoParcial.Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SegundoParcial.Server.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var conexion = builder.Configuration.GetConnectionString("conexion");
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlite(conexion));
 
 var app = builder.Build();
 
