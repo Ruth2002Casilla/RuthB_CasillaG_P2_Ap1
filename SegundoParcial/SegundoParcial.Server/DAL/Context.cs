a4bd6f8cca3c7b18aaac96596d424885f071ea43
Ruth2002Casilla <ruth_casilla@ucne.edu.do>
2024-03-04 19:46:15 -0400

    Contexto hecho

--- SegundoParcial/SegundoParcial.Server/DAL/Context.cs
+++ SegundoParcial/SegundoParcial.Server/DAL/Context.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿
+using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SegundoParcial.Server.DAL
 {
     public class Context : DbContext
     {
         public Context(DbContextOptions<Context>options) : base(options) { }
+
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+        public DbSet<Accesorios> Accesorios { get; set;}
+        public DbSet<VehiculoDetalle> VehiculosDetalle { get; set; }
     }
 }
